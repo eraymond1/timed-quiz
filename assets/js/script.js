108bd481f39e9b1c9543a9f9d030e0556a89decc
eraymond1 <ericraymond@rocketmail.com>
2022-04-23 23:24:09 -0400

    added object data to DOM and logic to tell when correct answer is clicked on

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var quizQuestions = [
 
     {
-        question: "Comonoly used data types DO Not Include:",
+        question: "Commonly used data types DO Not Include:",
         a: "strings",
         b: "booleans",
         c: "alerts",
@@ -37,193 +37,75 @@ var quizQuestions = [
     },
 ];
 
+var currentQuiz = 0;
+var quizScore = 0;
+
+var quizContainer = document.getElementById("quiz");
+var displayQuestion = document.getElementById("questionText");
+const answersEl = document.querySelectorAll(".answer");
+var answerLine = document.getElementById("answer-line");
+const click = document.querySelectorAll(".btn-answer");
+
+var posAnswer_a = document.getElementById("a");
+var posAnswer_b = document.getElementById("b");
+var posAnswer_c = document.getElementById("c");
+var posAnswer_d = document.getElementById("d");
 
 
 
-//var li = document.createElement("li");
-//var li = document.createElement("li");
-// var button = document.querySelector("button");
-
-// button.addEventListener("click", quizQuestions);
-
-
-    //document.getElementById("answer-line").innerHTML = answer;
 
 
 
+quiz();
 
-// function quizQuestions () {
-//     var q1 = "Comonoly used data types DO Not Include:"
-//     var answer1 = ["strings", "booleans", "alerts", "numbers"];
+function quiz() {
 
-//     var q2 = "Inside which HTML element do we put the JavaScript?"
-//     var answer2 = ["<scripting>", "<script>", "<js>", "<javascript"];
+var runQuiz = quizQuestions[currentQuiz];
+console.log(runQuiz);
 
-//     var q3 = "What is the correct JavaScript syntax to change the content of the HTML element below?";
-//     var answer3 = ["document.getElement('p').innerHTML = 'Hello World!';", "#demo.innerHTML = 'Hello Word!';",
-//      "document.getElementById('demo').innerHTML = 'Hello World!';", "document.getElementByName('p').innerHTML = 'Hello World!';"];
+//document.getElementById("question").innerHTML = text;
+  displayQuestion.innerText = runQuiz.question;
+  console.log(runQuiz.question);
+    posAnswer_a.innerHTML = runQuiz.a;
+    posAnswer_b.innerHTML = runQuiz.b;
+    posAnswer_c.innerHTML = runQuiz.c;
+    posAnswer_d.innerHTML = runQuiz.d;
 
-//      var q4 = "Where is the correct place to insert a JavaScript?";
-//      var answer4 = ["The <body> section", "The <head> section", "Both the <head> section and the <body> section are correct", "The <header> section"];
-//     var con = false;
-//     var answer;
+    if(answersEl.click){
 
-// // var btnA = document.createElement("button");
-// // btnA.innerText = answer1[0];
-// // document.getElementById("a").appendChild(btnA);
-
-// // var btnB = document.createElement("button");
-// // btnB.innerText = answer1[1];
-// // document.getElementById("b").appendChild(btnB);
-
-// // var btnC = document.createElement("button");
-// // btnC.innerText = answer1[2];
-// // document.getElementById("c").appendChild(btnC);
-
-// // var btnD = document.createElement("button");
-// // btnD.innerText = answer1[3];
-// // document.getElementById("d").appendChild(btnD);
-
-
-
-//     for (var i = 0; i < answer1.length; i++) {
-//         btnName = answer1[i];
-//         document.getElementsByClassName("answers").innerText = btnName;
-//         //document.getElementsByName(button).innerHTML = answer1[i];
-//         // document.getElementById("1").innerText = answer1[0];
-//         // document.getElementById("2").innerText = answer1[1];
-//         // document.getElementById("3").innerText = answer1[2];
-//         // document.getElementById("4").innerText = answer1[3];
-//         //document.getElementsByClassName("btn-ans").appendChild(button);
-
-        
-//             answer = answer1[2];
-//         if (!answer){
-//             answer = "Incorrect!";
+        answer =answersEl.id;
+        if (answer === runQuiz.ans){
+            answerLine.innerHTML = "Correct!";
+        }else {
+            answerLine.innerHTML = "Inccorect!";
+        }
+    }
             
-           
+                
+    [posAnswer_a, posAnswer_b, posAnswer_c, posAnswer_d].forEach(element => {
+        element.addEventListener("click", (e)=> {
+            // console.log(e);
+            var answer;
+            answer = element.id;
+            console.log(element);
+            if (answer === runQuiz.ans) {
+                answerLine.innerHTML = "Correct!";
             
-//         } else {
-//             // button.addEventListener("click", event => {
-//             //     document.getElementById("answer-line").innerHTML = answer;
-//             // })
-//             answer = "Correct!";
-//             //document.getElementById("answer-line").innerHTML = answer;
-//         }
-//     }
-
-
-
-
-
-
-
-
-
-// //ul.appendChild(li);
-// //debugger;
-// // switch(con) {
-// //     case btnA: 
-
-// //     if (btnA) {
-// //         con = false;
-// //         answer = "Incorrect!"
-
-// //     btnA.addEventListener("click", event => {
-
-// //         document.getElementById("answer-line").innerHTML = answer;
-// //         //btnA.textContent = `New question`;
-// //     });
-
-// //     }
     
+            }else {
+            
+                answerLine.innerHTML = "Incorrect!";
+            }
+        })
+    })
 
-// //     break;
-
-// //     case btnB:
-
-// //         answer = "Incorrect!"
-
-// //         btnB.addEventListener("click", event => {
-    
-// //             document.getElementById("answer-line").innerHTML = answer;
-// //         });
-
-// //         break;
-
-// //     case btnC:
-
-// //         answer = "Correct!"
-
-// //         btnC.addEventListener("click", event => {
-
-// //             document.getElementById("answer-line").innerHTML = answer;
-// //         });
-// //         break;
-
-// //     case btnD:
-
-// //         answer = "Incorrect!"
-
-// //         btnD.addEventListener("click", event => {
-    
-// //             document.getElementById("answer-line").innerHTML = answer;
-// //         });
-
-// //         break;
-
-// //         default :
-// //         break;
-// // }
-
-// //return answer;
-
-// // if (btnA) {
-// //     answer = "Incorrect!"
-
-// //     btnA.addEventListener("click", event => {
-
-// //         document.getElementById("answer-line").innerHTML = answer;
-// //         //btnA.textContent = `New question`;
-// //     });
-    
-// // } 
-// // else if(btnB) {
-// //     answer = "Incorrect!"
-
-// //     btnB.addEventListener("click", event => {
-
-// //         document.getElementById("answer-line").innerHTML = answer;
-// //     });
-
-// // }
-
-// // else if(btnC) {
-// //     answer = "Correct!"
-
-// //     btnC.addEventListener("click", event => {
-
-// //         document.getElementById("answer-line").innerHTML = answer;
-// //     });
-
-// // }
+};
 
 
 
-    
-
-    
-// //     btnD.addEventListener("click", event => {
-
-// //     btnD.textContent = `New question`;
-// // });
-
-// // for (var i = 0; i < answer1.length; i++){
-
-// // }
 
 
 
-// };
 
-//quizQuestions();
+
+
