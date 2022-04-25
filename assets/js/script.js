d6112bbddb3449f900f511b66df8a13c5465d265
eraymond1 <ericraymond@rocketmail.com>
2022-04-24 23:34:59 -0400

    adding code I have

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,19 +37,64 @@ var quizQuestions = [
     },
 ];
 
+
+
+
+
 var currentQuiz = 0;
 var quizScore = 0;
+
+var startText = document.getElementById("begin-text");
+var startButton = document.getElementById("start-btn");
+//var quizAnswer = document.getElementsByClassName(".answer-block").style.visibility = "hidden";
 
 var quizContainer = document.getElementById("quiz");
 var displayQuestion = document.getElementById("questionText");
 var answerLine = document.getElementById("answer-line");
+
+var a = document.getElementById("a-answer").style.visibility = "hidden";
+var b = document.getElementById("b-answer").style.visibility = "hidden";
+var c = document.getElementById("c-answer").style.visibility = "hidden";
+var d = document.getElementById("d-answer").style.visibility = "hidden";
 
 var posAnswer_a = document.getElementById("a");
 var posAnswer_b = document.getElementById("b");
 var posAnswer_c = document.getElementById("c");
 var posAnswer_d = document.getElementById("d");
 
-quiz();
+var initialForm = document.getElementById("save-initials").style.visibility = "hidden";
+var initialName = document.getElementById("l-name");
+var textField = document.getElementById("initials").value;
+var subBtn = document.getElementById("sub-btn");
+
+
+document.body.style.backgroundColor = "none";
+
+startText.innerHTML = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
+startButton.innerHTML = " Start Quiz";
+
+
+startButton.addEventListener("click", function(){
+    
+    quiz();
+
+    a = document.getElementById("a-answer").style.visibility = "visible";
+    b = document.getElementById("b-answer").style.visibility = "visible";
+    c = document.getElementById("c-answer").style.visibility = "visible";
+    d = document.getElementById("d-answer").style.visibility = "visible";
+
+    startButton = document.getElementById("start-btn").style.visibility = "hidden";
+    startText = document.getElementById("begin-text").style.visibility = "hidden";
+
+    var fiveMinutes = 60 * 5,
+    display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+    
+})
+
+
+//quiz();
+
 
 
 function quiz() {
@@ -88,6 +133,8 @@ var runQuiz = quizQuestions[currentQuiz];
 
         currentQuiz++;
 
+        
+
         if (currentQuiz < quizQuestions.length) {
             setTimeout(() => {
                 answerLine.innerHTML= "";
@@ -96,8 +143,25 @@ var runQuiz = quizQuestions[currentQuiz];
             
            
         } else {
-            answerLine.innerHTML = "The quiz has ended and your score is: " + quizScore;
+            answerLine.innerHTML = "The quiz has ended and your final score is: " + quizScore;
             console.log("quiz over, your score is " + quizScore);
+
+            initialForm = document.getElementById("save-initials").style.visibility = "visible";
+            initialName.innerHTML = "Enter Initials:";
+            //saveInitials();
+
+            subBtn.addEventListener("click", function() {
+                
+                    
+                    //textField = document.getElementById("initials").value;
+                   // return capture;
+                  // localStorage.setItem("initials", textField);
+
+                //console.log(capture);
+                
+               
+                
+            })
         }
         
 
@@ -109,13 +173,27 @@ var runQuiz = quizQuestions[currentQuiz];
 
 
 
+//cite author robbmj https://stackoverflow.com/questions/20618355/how-to-write-a-countdown-timer-in-javascript
+// the below code was referenced from robbmj from stack overflow
 
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
+        display.textContent = minutes + ":" + seconds;
 
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
 
-    
-   
+}
+
 
 
 
